1ec1d81d6ad1224a6be344659fb3ba477eb67fcc
ZibaloV <fotoroom.md@gmail.com>
2020-04-07 06:24:04 +0300

    Revert "make add course in db"
    This reverts commit 467b1ba7f85acd5fb89c612f947be0e94e5b26a0.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require( 'express' )
 const path = require( 'path' )
 const exphbs  = require( 'express-handlebars' )
-const mongoose = require( 'mongoose')
+const mongoose = require( 'mongoose');
 
 // =====ADD ROUTE PAGE=====
 const homePage = require('./routes/home')
--- models/Course.js
+++ models/Course.js
@@ -13,7 +13,38 @@ const course = new Schema ({
   image: {
     type: String
   }
-})
 
+  async saveCourse () { 
+    const getCourses = await Course.readAllCourse ()
+    const courseAll = JSON.parse ( getCourses )
+    courseAll.push({
+      id: this.id,
+      course: this.course,
+      price: this.price,
+      img: this.img 
+    })
+    return new Promise (
+      (resolve, reject) => {
+        fs.writeFile ( 
+          path.join ( __dirname, '..', 'data', 'courses.json'),
+          JSON.stringify ( courseAll ),
+          ( err ) => {
+           if ( err ) { reject ( err )}
+            else { resolve ( console.log ('write  resolveOK') ) } 
+           }  
+        )
+      }
+    )
+  }
 
+  static async getById (id) {
+    const courses = JSON.parse ( await Course.readAllCourse () )
+    return courses.find( c => c.id === id )
+  }
+}
+
+<<<<<<< HEAD
 module.exports = model ( 'CourseRepiad', course ) // make model/ his want constructor param of shema
+=======
+module.exports = Course
+>>>>>>> parent of 467b1ba... make add course in db
